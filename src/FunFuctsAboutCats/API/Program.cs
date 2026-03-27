e7b3e72f48b483bf4eab6427151ab32f17286924
wojciaas <wojtassregula@gmail.com>
2026-03-27 14:06:23 +0100

    serilog configuration

--- src/FunFuctsAboutCats/API/Program.cs
+++ src/FunFuctsAboutCats/API/Program.cs
@@ -1,21 +1,72 @@
 using Scalar.AspNetCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+        path: "logs/startup-logs-.json",
+        rollingInterval: RollingInterval.Day, 
+        formatter: new CompactJsonFormatter()
+    )   
+    .CreateBootstrapLogger();
 
-builder.Services.AddOpenApi();
+try
+{
+    Log.Information("Starting web host");
+    
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services));
 
-app.MapOpenApi();
-app.MapScalarApiReference();
+    builder.Services.AddOpenApi();
 
-var api = app.MapGroup("api")
-    .WithTags("Fun facts about cats");
+    var app = builder.Build();
 
-api.MapGet("/health", () => Results.Ok("Healthy"))
-    .WithTags("Health");
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 
-var facts = api.MapGroup("facts")
-    .WithTags("Facts");
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+        };
 
-app.Run();
+        options.GetLevel = (httpContext, elapsed, _) =>
+        {
+            if (httpContext.Request.Path.StartsWithSegments("/api/health"))
+            {
+                return LogEventLevel.Verbose;
+            }
+
+            return elapsed > 500
+                ? LogEventLevel.Warning
+                : LogEventLevel.Information;
+        };
+    });
+    
+    app.UseHttpsRedirection();
+
+    var api = app.MapGroup("api")
+        .WithTags("Fun facts about cats");
+
+    api.MapGet("/health", () => Results.Ok("Healthy"))
+        .WithTags("Health");
+    
+    var facts = api.MapGroup("facts")
+        .WithTags("Facts");
+    
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Server terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
